051d8adacac9bdce0a98ffaffcf7b552ef240082
LautaroRojasTorrilla <lautaro.rojas.torrilla@gmail.com>
2024-03-10 14:41:57 -0300

    Creacion Controllers

---
+++ Proyecto_Final_API/Controllers/NombreController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_Final_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NombreController : Controller
+    {
+        List<string> list = new List<string>() { "Lautaro", "Pepe", "Juan" };
+
+        [HttpGet]
+        public string ObtenerNombre()
+        {
+            return "Lautaro Rojas Torrilla";
+        }
+
+        [HttpGet("listado")]
+        public List<string> ObtenerListaDeNombres()
+        {
+            return this.list;
+        }
+
+        [HttpGet("listado/{id}")]
+        public ActionResult<string> ObtenerNombrePorId(int id)
+        {
+            if (id < 0 || id >= list.Count)
+            {
+                return BadRequest(new { mensaje = "Numero no válido", status = 400 });
+            }
+
+            return this.list[id];
+        }
+    }
+}
---
+++ Proyecto_Final_API/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto_Final_API.Models;
+using Proyecto_Final_API.Service;
+
+namespace Proyecto_Final_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UsuarioController : Controller
+    {
+        private UsuarioService usuarioService;
+
+        public UsuarioController(UsuarioService usuarioService)
+        {
+            this.usuarioService = usuarioService;
+        }
+
+        [HttpGet("listado")]
+        public IActionResult ObtenerListadoDeUsuarios()
+        {
+            List<Usuario> usuarios = this.usuarioService.ObtenerTodosLosUsuarios();
+
+            if (usuarios != null && usuarios.Any())
+            {
+                return Ok(usuarios);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+    }
+}
